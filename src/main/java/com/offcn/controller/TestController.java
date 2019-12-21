9503ed2da3145f9e333137c6391665d1b2f03511
hdj <1186182258@qq.com>
2019-12-21 21:36:58 +0800

    右侧提交

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -2,6 +2,6 @@ package com.offcn.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("左侧");
+        System.out.println("右侧");
     }
 }
